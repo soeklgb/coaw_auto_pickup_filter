f91a6d3cbfb5ce69e21d1706e310f166bee0eb6f
enzaki <96245674+soeklgb@users.noreply.github.com>
2024-06-23 13:09:00 +0900

    format

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,11 @@
 function isSkillBookNameWithoutLevel(name: string): boolean {
-    return /.+のスキル書$/.test(name);
+  return /.+のスキル書$/.test(name);
 }
 
 function removeLevelFromSkillBookName(name: string): string | undefined {
-    const result = /(.+のスキル書)\(Lv\d+(\.\d)?\)$/.exec(name);
-    if (result === null) return undefined;
-    return result[1];
+  const result = /(.+のスキル書)\(Lv\d+(\.\d)?\)$/.exec(name);
+  if (result === null) return undefined;
+  return result[1];
 }
 
 export { isSkillBookNameWithoutLevel, removeLevelFromSkillBookName };
