fde1fdf60c0eeab35f6b4392fb28cdd0c103efee
enzaki <96245674+soeklgb@users.noreply.github.com>
2024-06-23 18:48:30 +0900

    Copying English README file

--- tasks/copy_assets.ts
+++ tasks/copy_assets.ts
@@ -1,5 +1,6 @@
 async function copyAssets(modPath: string) {
   await Deno.copyFile("./README.md", `${modPath}/README.md`);
+  await Deno.copyFile("./README_en.md", `${modPath}/README_en.md`);
 }
 
 export { copyAssets };
